3cd524a7e1eeb20a2ce6be6fc9bcedbc9cbe7948
angelaahu <angelahu618@gmail.com>
2026-02-18 13:57:23 +0100

    corrected exercises

--- S01/count.py
+++ S01/count.py
@@ -1,3 +1,5 @@
+#Session 1. Exercise 2
+
 for i in range(1,21):
-    print(i)
+    print(i, end=' ')
 
--- S01/sum20.py
+++ S01/sum20.py
@@ -1,2 +1,12 @@
-total_sum = sum(range(1, 21))
-print("The sum of of the first 20 integers is:", total_sum)
+#Session 1. Exercise 3
+#Calculate sum of the 20 first int numbers
+#1 + 2 + 3 + ... + 20
+#Example working!
+
+#--- Store the result
+res = 0
+
+for i in range(1, 21):
+    res += i
+
+print("The sum of of the first 20 integers is:", res)

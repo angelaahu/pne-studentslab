bbdc58dcfe6f23cbd919c9a44212cf7c47450f7c
angelaahu <angelahu618@gmail.com>
2026-04-08 17:08:15 +0200

    Practical exercises

--- Guess the Number/NumberGuesserClass.py
+++ Guess the Number/NumberGuesserClass.py
@@ -1,23 +1,66 @@
+import socket
+import random
+
 class NumberGuesser:
     def __init__(self, secret_number = None, attempts = None):
         self.secret_number = secret_number
         self.attempts = attempts
 
     def guess(self, number):
-        count = 0
-        if number != secret_numer:
-            count += 1
-        if number == secret_number:
-            result = f"You won after {count} attempts"
-        elif number > secret_number:
+        #result = 0
+        #if number != self.secret_number:
+         #   count += 1
+        if number == self.secret_number:
+            result = f"You won after {len(self.attempts)} attempts"
+        elif number > self.secret_number:
             result = "Lower"
-        elif number < secret_number:
+        elif number < self.secret_number:
             result = "Higher"
-
         return result
 
     def __str__(self):
-        return self.attempts
+        return self.secret_number, self.attempts
 
 
+ls = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+ls.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
+IP = "127.0.0.1"
+PORT = 8081
+
+ls.bind((IP, PORT))
+ls.listen()
+
+n = random.randint(1, 100)
+attempts_list = []
+
+while True:
+    print("Waiting for Clients to connect")
+    try:
+        (cs, client_ip_port) = ls.accept()
+
+    except KeyboardInterrupt:
+        print("Game stopped by the user")
+        ls.close()
+        exit()
+
+    else:
+        print("A client has connected to the game!!")
+        print(n)
+        game= NumberGuesser(n, attempts_list)
+        msg_raw = cs.recv(2048)
+        msg = msg_raw.decode()
+
+        att_n = int(msg)
+        attempt, attempts_list  = game.guess(att_n)
+        if len(attempts_list) <= 20:
+            send = attempt + "\n" + f"Numbers tried: {attempts_list}"
+            cs.send(send.encode())
+            if attempt != "lower" and attempt != "Higher":
+                cs.close()
+
+        else:
+            response = "You are too bad for the game"
+            cs.send(response.encode())
+            cs.close()
+        
--- Guess the Number/Player.py
+++ Guess the Number/Player.py
@@ -1,1 +1,16 @@
 import socket
+
+PORT = 8081
+IP = "127.0.0.1"
+
+print(f"Connection to SERVER at {IP}, PORT: {PORT}")
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+s.connect((IP,PORT))
+
+s.send(str.encode(msg)
+response = s.recv(2048).decode("utf-8")
+s.close()
+
